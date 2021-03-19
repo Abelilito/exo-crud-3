8bacf0fde65ed4173268d1c90ef4e90b2ef5fb5f
Abel Ilito <ilitoabel@gmail.com>
2021-03-19 12:57:40 +0100

    Add seed with gem faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "faker"
+
+5.times do
+  restaurants = Restaurant.new(
+    name: Faker::Name.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+    category: Faker::Restaurant.type
+  )
+  restaurants.save
+end
